a99087394f3b2e92bdb4ab9e8dbd4f082750057b
vishnuprashanth123 <119410125+vishnuprashanth123@users.noreply.github.com>
2022-12-04 02:39:26 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -1,8 +1,8 @@
 version=$Build_ID 
 sudo bash
-docker build -t gitrepos:$version .
+docker build -t ecrrepo:$version .
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 198095598917.dkr.ecr.ap-south-1.amazonaws.com
-docker tag gitrepos:$version 198095598917.dkr.ecr.ap-south-1.amazonaws.com/gitrepos:$version
-docker push 198095598917.dkr.ecr.ap-south-1.amazonaws.com/gitrepos:$version
+docker tag ecrrepo:$version 198095598917.dkr.ecr.ap-south-1.amazonaws.com/ecrrepo:$version
+docker push 198095598917.dkr.ecr.ap-south-1.amazonaws.com/ecrrepo:$version
 
 echo "build docker image to ECR"
