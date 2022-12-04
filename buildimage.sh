79a27e2cae6ae2b7a6acd680d62fe50dc0e366d5
vishnuprashanth123 <119410125+vishnuprashanth123@users.noreply.github.com>
2022-12-04 19:01:56 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -5,3 +5,9 @@ docker tag ecrrepo:$version 198095598917.dkr.ecr.ap-south-1.amazonaws.com/ecrrep
 docker push 198095598917.dkr.ecr.ap-south-1.amazonaws.com/ecrrepo:$version
 
 echo "build docker image to ECR"
+#create new task definition with buildiD
+
+sed -i s#198095598917.dkr.ecr.ap-south-1.amazonaws.com/ecrrepo:8#198095598917.dkr.ecr.ap-south-1.amazonaws.com/ecrrepo:$version#g taskdef.json
+aws ecs register-task-definition --CLI-input-json file://taskdef.json --region ap-south-1
+
+echo "new task created"
