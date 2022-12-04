c03b93205d6683671ecc5939c35ce8aafbb0b68c
vishnuprashanth123 <119410125+vishnuprashanth123@users.noreply.github.com>
2022-12-04 13:31:30 +0530

    Update buildimage.sh

--- buildimage.sh
+++ buildimage.sh
@@ -1,4 +1,4 @@
-version=$Build_ID 
+version=$BUILD_ID 
 docker build -t ecrrepo:$version .
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 198095598917.dkr.ecr.ap-south-1.amazonaws.com
 docker tag ecrrepo:$version 198095598917.dkr.ecr.ap-south-1.amazonaws.com/ecrrepo:$version
